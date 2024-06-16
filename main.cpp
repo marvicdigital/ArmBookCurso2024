4fc0375e945f2b4dec7b549498fbf640368f35a4
marvicdigital <marvicdigital@gmail.com>
2024-06-16 13:29:03 +0000

    Leds estan en Puerto A

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     DigitalIn B1_USER(BUTTON1);
 
-    DigitalOut LD1(LED1);
+    DigitalOut LD1(LED1);   //Leds conectados a puerto A LD1 es PA5
 
     while (true) {
         LD1 = B1_USER;
